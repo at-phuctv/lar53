56c03aa38fd366490f32593ae91cd51fe1959a4a
at-tienhx <tienhx@asiantech.vn>
2016-09-22 17:10:04 +0700

    custom route and layouts template

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,17 @@
   |
  */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', function () {
+        return view('home');
+    });
+    Route::get('/home', 'HomeController@index');
+    Route::get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::get('logout', 'Auth\LoginController@logout');
 });
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
-Route::get('login', 'Auth\LoginController@showLoginForm');
-Route::post('login', 'Auth\LoginController@login');
-Route::get('register', 'Auth\RegisterController@showRegistrationForm');
-Route::post('register', 'Auth\RegisterController@register');
-Route::get('logout', 'Auth\LoginController@logout');
