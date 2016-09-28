7ef2db854e141592229e9e9b568af395cbb2f9bc
at-tienhx <tienhx@asiantech.vn>
2016-09-28 15:09:22 +0700

    fix migrate category

--- database/migrations/2016_09_19_073543_create_t_category_table.php
+++ database/migrations/2016_09_19_073543_create_t_category_table.php
@@ -16,6 +16,7 @@ class CreateTCategoryTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('introduce');
+            $table->string('image');
             $table->timestamps();
         });
     }
